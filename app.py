984016d69c3e9270963dc49b2f72cfd1779fb0a1
HEBECTA <40593462+HEBECTA@users.noreply.github.com>
2021-03-17 08:50:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def get_all():
 # curl http://localhost:5000/songs -X GET
 
 @app.route('/songs/<song_id>', methods=['GET'])
-def fetch(song_id):
+def get_song(song_id):
     song = Songs.query.get_or_404(song_id)
     output = []
     currSong = {}
@@ -60,7 +60,7 @@ def fetch(song_id):
 # curl http://localhost:5000/songs/1 -X GET
 
 @app.route('/songs', methods=['POST'])
-def add():
+def add_song():
     songData = request.get_json()
     date = datetime.strptime(songData['date_created'], "%Y-%m-%d")
     song = Songs(artist=songData['artist'], name=songData['name'], date_created=date, link=songData['link'])
@@ -72,7 +72,7 @@ def add():
 
 
 @app.route('/songs/<song_id>', methods=['DELETE'])
-def delete(song_id):
+def delete_song(song_id):
     song = Songs.query.get_or_404(song_id)
     output = []
     currSong = {}
@@ -89,7 +89,7 @@ def delete(song_id):
 # curl http://localhost:5000/songs/12 -X DELETE
 
 @app.route('/songs/<song_id>', methods=['PUT'])
-def edit(song_id):
+def edit_song(song_id):
     song = Songs.query.get_or_404(song_id)
     songData = request.get_json()
     song.artist = songData['artist']
@@ -108,6 +108,52 @@ def edit(song_id):
     return jsonify(output)
 
 # curl http://localhost:5000/songs/12 -d '{"name":"daina2", "artist":"muzikantas2", "date_created":"2018-02-03", "link":"www.google.com"}' -H "Content-Type: application/json" -X PUT
+
+
+###############################################################################################################
+# F R O N T   E N D
+
+@app.route('/')
+def index():
+    songs = Songs.query.all()
+    return render_template('index.html', songs=songs)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        artist = request.form["artist"]
+        name = request.form["name"]
+        #print("DATA ATSPAUSDINTA")
+        #print(request.form["date"])
+        date = datetime.strptime (request.form["date"], "%Y-%m-%d")
+        link = request.form["link"]
+        data = Songs(artist=artist, name=name, date_created=date, link=link)
+        db.session.add(data)
+        db.session.commit()
+        return redirect("/")
+    else:
+        return render_template('add.html')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    song = Songs.query.get_or_404(id)
+    if request.method == 'POST':
+        song.artist = request.form["artist"]
+        song.name = request.form["name"]
+        song.date_created = datetime.strptime (request.form["date"], "%Y-%m-%d")
+        song.link = request.form["link"]
+        db.session.commit()
+        return redirect("/")
+    else:
+        return render_template('edit.html', song=song)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    song = Songs.query.get_or_404(id)
+    db.session.delete(song)
+    db.session.commit()
+    return redirect('/')
 
 
 if __name__ == "__main__":
